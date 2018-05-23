1c9c16af61c7b672fd0af9788e2763d54ad1d122
Ubuntu <ajay@ubu.hjhxekgtdhbezohmvbwqdmhknb.bx.internal.cloudapp.net>
2018-05-23 04:24:48 +0000

    auth

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const abcd = require('./abcd/abcd.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(abcd);
+  app.configure(users);
 };
